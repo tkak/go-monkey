479c9b8165ac60fc30080a26b1fc73770ac569e0
Takaaki Furukawa <takaaki.frkw@gmail.com>
2018-11-04 15:20:52 +0900

    Add error for parser

--- monkey/parser/parser.go
+++ monkey/parser/parser.go
@@ -1,6 +1,8 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/tkak/go-monkey/monkey/ast"
 	"github.com/tkak/go-monkey/monkey/lexer"
 	"github.com/tkak/go-monkey/monkey/token"
@@ -9,12 +11,16 @@ import (
 type Parser struct {
 	l *lexer.Lexer
 
+	errors    []string
 	curToken  token.Token
 	peekToken token.Token
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
 
 	p.nextToken()
 	p.nextToken()
@@ -84,6 +90,16 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.nextToken()
 		return true
 	} else {
+		p.peekError(t)
 		return false
 	}
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
